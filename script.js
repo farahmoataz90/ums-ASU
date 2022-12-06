eb67d402ad05b7ae5b4855ea6a82c8ba292abdf0
Farah Moataz <farahmoataz90@gmail.com>
2022-12-06 11:31:27 +0200

    updated

---
+++ script.js
@@ -0,0 +1,67 @@
+let number = document.getElementById("number1");
+let counter = 0;
+setInterval(()=>{
+    if(counter == 19){
+        clearInterval();
+    }
+    else{
+        counter+=1;
+        number.innerHTML = counter + "+";
+    }
+    
+}, 100)
+
+
+
+
+let number2 = document.getElementById("number2");
+let counter2 = 1229000;
+setInterval(()=>{
+    if(counter2 == 1229145){
+        clearInterval();
+    }
+    else{
+        counter2+=1;
+        number2.innerHTML = counter2 ;
+    }
+    
+}, 10)
+
+
+
+
+
+let number3 = document.getElementById("number3");
+let counter3 = 220600;
+setInterval(()=>{
+    if(counter3 == 220614){
+        clearInterval();
+    }
+    else{
+        counter3+=1;
+        number3.innerHTML = counter3 + "+";
+    }
+    
+}, 100)
+
+
+
+
+
+
+let number4 = document.getElementById("number4");
+let counter4 = 220500;
+setInterval(()=>{
+    if(counter4 == 220540){
+        clearInterval();
+    }
+    else{
+        counter4+=1;
+        number4.innerHTML = counter4 + "+";
+    }
+    
+}, 50)
+
+
+
+
